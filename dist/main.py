051fb9c75654cbe98a1d6ce2b807d5be3f66e0fd
marouenes <marouane.skandaji@gmail.com>
2021-08-04 18:08:47 +0200

    Setting up the virtual environment in the same directory as the Pipfile

---
+++ dist/main.py
@@ -0,0 +1,23 @@
+# <-- Encoding - utf8 -->
+
+import os
+import sys
+
+if os.path.exists('src.zip'):
+    sys.path.insert(0, 'src.zip')
+else:
+    sys.path.insert(0, './src')
+
+from utilities import app
+
+if __name__ == '__main__':
+    
+    from pyspark.sql import SparkSession
+
+    spark = SparkSession.builder \
+        .master("local") \
+        .appName("Test") \
+        .getOrCreate()
+    
+    app.say_hello('John')
+    print(spark.range(10000).where("id > 1000").selectExpr("sum(id)").collect())
